45c55d92bb91bd92e3dd94e93af8cb0f87a779c7
Expired <imoses2@hotmail.com>
2022-01-18 00:03:55 -0700

    handle case for more than 30 issues

--- assets/js/single.js
+++ assets/js/single.js
@@ -1,7 +1,7 @@
 //variables
 //==================================
 var issueContainerEl = document.querySelector("#issues-container");
-
+var limitWarningEl = document.querySelector("#limit-warning");
 
 
 
@@ -17,8 +17,10 @@ var getRepoIssues = function(repo) {
 		if (response.ok) {
 			response.json()
 			.then(function(data) {
-				console.log(data);
 				displayIssues(data);
+				if (response.headers.get("Link")) {
+					displayWarning(repo);
+				}
 			});
 		} else {
 			alert("There was a problem with your request!");
@@ -63,7 +65,18 @@ var displayIssues = function(issues) {
 	
 };
 
+var displayWarning = function(repo) {
+  // add text to warning container
+  limitWarningEl.textContent = "To see more than 30 issues, visit ";
 
+	var linkEl = document.createElement("a");
+  linkEl.textContent = "See More Issues on GitHub.com";
+  linkEl.setAttribute("href", `https://github.com/${repo}/issues`);
+  linkEl.setAttribute("target", "_blank");
+
+  // append to warning container
+  limitWarningEl.appendChild(linkEl);
+};
 
 
 
@@ -83,8 +96,8 @@ var displayIssues = function(issues) {
 //body
 //=====================================
 // getRepoIssues("moses-ian/git-it-done");
-getRepoIssues("moses-ian/code-quiz");
-// getRepoIssues("facebook/react");
+// getRepoIssues("moses-ian/code-quiz");
+getRepoIssues("facebook/react");
 
 
 
